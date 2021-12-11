a4fbfceb00bcff40e470f6e17c42cecac67985c9
mj <mj@Marks-Mac-mini.local>
2021-12-10 18:24:26 -0700

    Added Sieve of Eratosthenes for primality testing

---
+++ Friendly.Library/Primes.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+
+namespace Friendly.Library
+{
+   public static class Primes
+   {
+      private static BigBitArray _primes;
+
+      private static long _upperSieveLimit = 0;
+
+      /// <summary>
+      /// Initializes the Prime checking by running the internal Sieve of Eratosthenes.
+      /// </summary>
+      /// <param name="ul">The highest number to be run in the Sieve.</param>
+      /// <remarks>This must be called prior to calling IsPrime.  If ul is not a multiple of 64, it will
+      /// be increased to the next multiple of 64.</remarks>
+      public static void Init(long ul)
+      {
+         _primes = SieveOfEratosthenes(ul);
+         _upperSieveLimit = _primes.Capacity;
+      }
+
+      /// <summary>
+      /// Determines if the given value is prime.
+      /// </summary>
+      /// <param name="n">The value to check for primality.</param>
+      /// <returns>True if the number if prime; false if the number is composite.</returns>
+      public static bool IsPrime(long n)
+      {
+#if DEBUG
+         if (n < 0)
+            throw new ArgumentOutOfRangeException($"{nameof(n)} must not be negative.");
+#endif
+
+         if (n < _upperSieveLimit)
+            return !_primes[n];
+         else
+            throw new ArgumentOutOfRangeException($"{nameof(n)} exceeds the upper limit of {_upperSieveLimit}");
+      }
+
+      /// <summary>
+      /// Returns a bit vector wherein each set bit corresponds to a composite number. 
+      /// </summary>
+      /// <param name="ul">The highest number (inclusive) to be sieved.</param>
+      /// <returns>a bit vector wherein each set bit corresponds to a composite number. </returns>
+      /// <remarks>False / clear bits indicate a prime number.</remarks>
+      private static BigBitArray SieveOfEratosthenes(long ul)
+      {
+         BigBitArray rv = new BigBitArray(ul + 1);
+
+         // Seed this with 0 and 1 as composite.
+         rv[0] = true;
+         rv[1] = true;
+
+         long jul = 1 + rv.Capacity / 2;
+         long index;
+         for (long j = 2; j < jul; j ++)
+         {
+            index = 2 * j;
+            while (index <= ul)
+            {
+               rv[index] = true;
+               index += j;
+            }
+         }
+
+         return rv;
+      }
+   }
+}
---
+++ Test.Library/TestPrimes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Friendly.Library;
+using Xunit;
+
+namespace Test.Library
+{
+   public class TestPrimes
+   {
+      // The upper limit of the sieve is set to one less than a multiple of 64,
+      // so we can use this constant in our tests.  This limit is inclusive
+      // of the given value.
+      private const int _upperLimit = 25_023;
+
+      private static bool _isInitialized = false;
+
+      public TestPrimes()
+      {
+         if (!_isInitialized)
+         {
+            Primes.Init(_upperLimit);
+            _isInitialized = true;
+         }
+      }
+
+      [Fact]
+      public void IsPrime_Throws()
+      {
+#if DEBUG
+         Assert.Throws<ArgumentOutOfRangeException>(() => Primes.IsPrime(-1));
+#endif
+         // Note: _upperLimit is inclusive, so we have to add one to get it
+         // to throw the Exception.
+         Assert.Throws<ArgumentOutOfRangeException>(() => Primes.IsPrime(_upperLimit + 1));
+      }
+
+      public static TheoryData<bool, long> IsPrimeTestData
+      {
+         get
+         {
+            var rv = new TheoryData<bool, long>();
+
+            rv.Add(true, 2);
+            rv.Add(true, 3);
+            rv.Add(false, 49);
+            rv.Add(false, 121);
+            rv.Add(true, 4999);
+            rv.Add(true, 25013);
+            rv.Add(false, 25023);
+
+            return rv;
+         }
+
+      }
+
+      [Theory]
+      [MemberData(nameof(IsPrimeTestData))]
+      public void IsPrime(bool expected, long value)
+      {
+         bool actual = Primes.IsPrime(value);
+
+         Assert.Equal(expected, actual);
+      }
+   }
+}
