d55cb6c86255a6901161b05f09eb869bb6752aaa
mj <mj@Marks-Mac-mini.local>
2021-12-14 14:27:17 -0700

    Added method to find the GCD

---
+++ Friendly.Library/LongCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Friendly.Library
+{
+   public static class LongCalculator
+   {
+      public static long GCD(long a, long b)
+      {
+#if DEBUG
+         if (a < b)
+            throw new ArgumentException($"{nameof(a)} must be greater than {nameof(b)}");
+#endif
+         if (b == 0)
+            return a;
+
+         return GCD(b, a % b);
+      }
+   }
+}
---
+++ Test.Library/TestLongCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Friendly.Library;
+using Xunit;
+
+namespace Test.Library
+{
+   public class TestLongCalculator
+   {
+      public static TheoryData<long, long, long> GCDTestData
+      {
+         get
+         {
+            var rv = new TheoryData<long, long, long>();
+
+            rv.Add(1, 7523, 1783);
+            rv.Add(47, 47 * 93923, 47 * 77849);
+
+            return rv;
+         }
+      }
+
+      [Theory]
+      [MemberData(nameof(GCDTestData))]
+      public void GCD(long expectedGcd, long a, long b)
+      {
+         long actualGcd = LongCalculator.GCD(a, b);
+
+         Assert.Equal(expectedGcd, actualGcd);
+      }
+
+      [Fact]
+      public void GCD_Throws()
+      {
+#if DEBUG
+         Assert.Throws<ArgumentException>(() => LongCalculator.GCD(512, 513));
+#endif
+      }
+   }
+}
