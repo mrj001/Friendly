1393de09ccb8a0b34845a7a4573201fe95669996
mj <mj@Marks-Mac-mini.local>
2021-12-20 18:17:36 -0700

    Added PrimeFactorization class

---
+++ Friendly.Library/PrimeFactorization.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Friendly.Library
+{
+   public class PrimeFactorization : IList<PrimeFactor>
+   {
+      private readonly List<PrimeFactor> _factors;
+
+      private PrimeFactorization(List<PrimeFactor> factors)
+      {
+         _factors = factors;
+      }
+
+      /// <summary>
+      /// Gets the Prime
+      /// </summary>
+      /// <param name="n"></param>
+      /// <returns></returns>
+      public static PrimeFactorization Get(long n)
+      {
+         IEnumerator<long> primes = Primes.GetEnumerator();
+         long nCopy = n;
+         long prime;
+         int exponent;
+         long quotient, remainder;
+         List<PrimeFactor> factors = new List<PrimeFactor>();
+
+         while (primes.MoveNext() && nCopy != 1)
+         {
+            prime = primes.Current;
+            exponent = 0;
+            quotient = Math.DivRem(nCopy, prime, out remainder);
+            while (remainder == 0)
+            {
+               nCopy = quotient;
+               exponent++;
+               quotient = Math.DivRem(nCopy, prime, out remainder);
+            }
+
+            if (exponent > 0)
+               factors.Add(new PrimeFactor(prime, exponent));
+         }
+
+         if (nCopy > 1)
+         {
+            if (Primes.IsPrime(nCopy))
+               factors.Add(new PrimeFactor(nCopy, 1));
+            else
+               // TODO attempt to factor remaining number
+               throw new ApplicationException($"Failed to find prime factorization of {n}; remaining composite: {nCopy}");
+         }
+
+         return new PrimeFactorization(factors);
+      }
+
+      public PrimeFactor this[int index]
+      {
+         get => _factors[index];
+         set => throw new NotSupportedException();
+      }
+
+      public int Count { get => _factors.Count; }
+
+      public bool IsReadOnly { get => true; }
+
+      public void Add(PrimeFactor item)
+      {
+         throw new NotSupportedException();
+      }
+
+      public void Clear()
+      {
+         throw new NotSupportedException();
+      }
+
+      public bool Contains(PrimeFactor item)
+      {
+         // TODO
+         throw new NotImplementedException();
+      }
+
+      public void CopyTo(PrimeFactor[] array, int arrayIndex)
+      {
+         _factors.CopyTo(array, arrayIndex);
+      }
+
+      public IEnumerator<PrimeFactor> GetEnumerator()
+      {
+         return _factors.GetEnumerator();
+      }
+
+      IEnumerator IEnumerable.GetEnumerator()
+      {
+         return _factors.GetEnumerator();
+      }
+
+      public int IndexOf(PrimeFactor item)
+      {
+         // TODO
+         throw new NotImplementedException();
+      }
+
+      public void Insert(int index, PrimeFactor item)
+      {
+         throw new NotSupportedException();
+      }
+
+      public bool Remove(PrimeFactor item)
+      {
+         throw new NotSupportedException();
+      }
+
+      public void RemoveAt(int index)
+      {
+         throw new NotSupportedException();
+      }
+   }
+}
---
+++ Test.Library/TestPrimeFactorization.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Friendly.Library;
+using Xunit;
+
+namespace Test.Library
+{
+   public class TestPrimeFactorization
+   {
+      public TestPrimeFactorization()
+      {
+         TestPrimes.EnsureInitialized();
+      }
+
+      public static TheoryData<(long factor, int exponent)[], long> GetPrimeFactorizationTestData
+      {
+         get
+         {
+            var rv = new TheoryData<(long factor, int exponent)[], long>();
+
+            rv.Add(new (long, int)[] { (2, 2), (7, 1)}, 28);
+            rv.Add(new (long, int)[] { (2, 4), (3, 1) }, 48);
+            rv.Add(new (long, int)[] { (7, 2) }, 49);
+            rv.Add(new (long, int)[] { (149, 2), (163, 2) }, 149 * 149 * 163 * 163);
+            rv.Add(new (long, int)[] { (7, 1) , (11, 1), (13, 1), (60149, 1) }, 7 * 11 * 13 * 60149);
+
+            return rv;
+         }
+      }
+
+      [Theory]
+      [MemberData(nameof(GetPrimeFactorizationTestData))]
+      public void GetPrimeFactorization((long factor, int exponent)[] expected, long n)
+      {
+         PrimeFactorization actual = PrimeFactorization.Get(n);
+
+         Assert.Equal(expected.Length, actual.Count);
+
+         for (int j = 0; j < expected.Length; j ++)
+         {
+            Assert.Equal(expected[j].factor, actual[j].Factor);
+            Assert.Equal(expected[j].exponent, actual[j].Exponent);
+         }
+      }
+
+      [Fact]
+      public void GetPrimeFactorization_Throws()
+      {
+         // TODO when factorization of larger integers is implemented, this
+         //   test needs to be removed.  This number has 2 prime factors larger
+         //   than the Sieve Limit (See TestPrimes._upperLimit).
+         //   Note that the product of these factors must be less than 55108**2
+         //   to avoid an overflow of the long data type during primality testing.
+         long n = 2 * 2 * 17 * 19 * 48661 * 55441L;
+
+         Assert.Throws<ApplicationException>(() => PrimeFactorization.Get(n));
+      }
+   }
+}
