957261fc22ae0c5c6d2a36b6ce6b65cf5bb35525
mj <mj@Marks-Mac-mini.local>
2021-12-25 21:13:08 -0700

    Added console app to get the prime factorization of a given number

---
+++ Factorize/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using Friendly.Library;
+
+namespace Factorize
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         long n;
+         if (!long.TryParse(args[args.Length - 1], NumberStyles.Integer | NumberStyles.AllowThousands, null, out n))
+         {
+            Console.WriteLine($"Unable to parse {args[args.Length - 1]}");
+            return;
+         }
+
+         Primes.Init(4_294_967_296);  // sufficient up to long.MaxValue
+         PrimeFactorization factors = PrimeFactorization.Get(n);
+
+         // Determine the number of characters needed to display the largest factor
+         // and the largest exponent.
+         int factorWidth = 0;
+         int powerWidth = 0;
+         foreach (PrimeFactor f in factors)
+         {
+            factorWidth = (int)Math.Max(Math.Ceiling(Math.Log10(f.Factor)), factorWidth);
+            powerWidth = (int)Math.Max(Math.Ceiling(Math.Log10(f.Exponent)), powerWidth);
+         }
+
+         // Add some space to allow for comma separators
+         factorWidth += (factorWidth - 1) / 3;
+         powerWidth += (powerWidth - 1) / 3;
+
+         Console.WriteLine($"The prime factors of {n} are:");
+         string fmt = $"{{0, {factorWidth}}} {{1, {powerWidth}}}";
+         Console.WriteLine(fmt, "Factor", "Power");
+         fmt = $"{{0,{factorWidth}:N0}} {{1,{powerWidth}:N0}}";
+         Console.WriteLine(fmt);    // debugging
+         foreach (PrimeFactor f in factors)
+            Console.WriteLine(fmt, f.Factor, f.Exponent);
+      }
+   }
+}
