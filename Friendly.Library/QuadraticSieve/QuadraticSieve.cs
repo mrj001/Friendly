67bf3f1d87c16a34a57fac49c8bd94dbedbfd205
mj <mj@Marks-Mac-mini.local>
2022-01-04 13:45:41 -0700

    Selecting a factor base for Quadratic Sieve

---
+++ Friendly.Library/QuadraticSieve/QuadraticSieve.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Friendly.Library;
+
+//====================================================================
+// References
+//====================================================================
+//
+// A. Kefa Rabah , 2006. Review of Methods for Integer Factorization
+//    Applied to Cryptography. Journal of Applied Sciences, 6: 458-481.
+//    https://scialert.net/fulltext/?doi=jas.2006.458.481
+
+namespace Friendly.Library.QuadraticSieve
+{
+   public static class QuadraticSieve
+   {
+      /// <summary>
+      /// Determines an appropriate factor base for factoring the given number
+      /// </summary>
+      /// <param name="n">The number being factored.</param>
+      /// <returns>A factor base to use for factoring the given number.</returns>
+      internal static List<long> FactorBase(long n)
+      {
+         double logn = Math.Log(n);
+         // Number of odd primes to consider for the Factor Base per Ref. A.
+         int sz = (int)Math.Floor(Math.Sqrt(Math.Exp(Math.Sqrt(logn * Math.Log(logn)))));
+         List<long> rv = new List<long>(sz);
+
+         // We can always include 2 because any odd number squared will have 1 as a
+         // Quadratic Residue modulo 2.
+         rv.Add(2);
+
+         // Add primes p such that (n % p) is a quadratic residue modulo p.
+         // Note that for primes in the second argument, the Jacobi Symbol
+         // reduces to the Legendre Symbol.
+         IEnumerator<long> j = Primes.GetEnumerator();
+         j.MoveNext();  // Skip 2
+         int k = 0;
+         while (j.MoveNext() && k < sz)
+         {
+            if (1 == LongCalculator.JacobiSymbol(n % j.Current, j.Current))
+               rv.Add(j.Current);
+            k++;
+         }
+
+         return rv;
+      }
+   }
+}
---
+++ Test.Library/QuadraticSieve/TestQuadraticSieve.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Friendly.Library;
+using Friendly.Library.QuadraticSieve;
+using Xunit;
+
+//====================================================================
+// References
+//====================================================================
+//
+// A. Kefa Rabah , 2006. Review of Methods for Integer Factorization
+//    Applied to Cryptography. Journal of Applied Sciences, 6: 458-481.
+//    https://scialert.net/fulltext/?doi=jas.2006.458.481
+
+
+namespace Test.Library.QuadraticSieve
+{
+   public class TestQuadraticSieve
+   {
+      public TestQuadraticSieve()
+      {
+         TestPrimes.EnsureInitialized();
+      }
+
+      public static TheoryData<long[], long> FactorBaseTestData
+      {
+         get
+         {
+            var rv = new TheoryData<long[], long>();
+
+            // This is taken from the Quadratic Sieve example in Ref. A.
+            rv.Add(new long[] { 2,3,7,11,17,19,23,37,41}, 45_313);
+
+            return rv;
+         }
+      }
+
+      [Theory]
+      [MemberData(nameof(FactorBaseTestData))]
+      public void FactorBase(long[] expected, long n)
+      {
+         List<long> actual = Friendly.Library.QuadraticSieve.QuadraticSieve.FactorBase(n);
+
+         Assert.Equal(expected.Length, actual.Count);
+         for (int j = 0; j < actual.Count; j++)
+            Assert.Equal(expected[j], actual[j]);
+      }
+   }
+}
