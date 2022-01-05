74e3862ad4a0646412ff7a117678e4057bec8983
mj <mj@Marks-Mac-mini.local>
2022-01-05 10:41:43 -0700

    Added a matrix class

---
+++ Friendly.Library/QuadraticSieve/Matrix.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Friendly.Library.QuadraticSieve
+{
+      /// <summary>
+      /// A matrix modulo 2.
+      /// </summary>
+   public class Matrix
+   {
+      private readonly List<BigBitArray> _rows;
+      private int _columns;
+
+      /// <summary>
+      /// Constructs a new Matrix object.
+      /// </summary>
+      /// <param name="rowSize">The number of rows in the new Matrix.</param>
+      /// <param name="columnSize">The number of columns in the new Matrix.</param>
+      /// <param name="columnBuffer">An allowance for optimized future expansion of the number of columns.</param>
+      /// <remarks>
+      /// <para>
+      /// All bits are initially set to zero (false).
+      /// </para>
+      /// </remarks>
+      public Matrix(int rowSize, int columnSize, int columnBuffer)
+      {
+         _rows = new List<BigBitArray>(rowSize);
+         _columns = columnSize;
+         for (int j = 0; j < rowSize; j++)
+            _rows.Add(new BigBitArray(columnSize + columnBuffer));
+      }
+
+      /// <summary>
+      /// Gets the number of Rows in the Matrix
+      /// </summary>
+      public int Rows { get => _rows.Count; }
+
+      /// <summary>
+      /// Gets the number of Columns in the Matrix.
+      /// </summary>
+      public int Columns { get => _columns; }
+
+      /// <summary>
+      /// Expands the number of Columns to the given amount.
+      /// </summary>
+      /// <param name="newColumnSize">The number of columns to expand to.</param>
+      /// <remarks>
+      /// Any attempt to shrink the number of columns is silently ignored.
+      /// </remarks>
+      public void ExpandColumns(int newColumnSize)
+      {
+         if (newColumnSize < _columns)
+            return;
+
+         if (newColumnSize < _rows[0].Capacity)
+         {
+            _columns = newColumnSize;
+            return;
+         }
+
+         foreach (BigBitArray row in _rows)
+            row.Expand(newColumnSize);
+         _columns = newColumnSize;
+      }
+
+      [Conditional("DEBUG")]
+      private void ValidateRow(int rowIndex)
+      {
+         if (rowIndex < 0 || rowIndex >= _rows.Count)
+            throw new ArgumentOutOfRangeException($"{nameof(rowIndex)} = {rowIndex} is out of bounds [0, {_rows.Count})");
+      }
+
+      [Conditional("DEBUG")]
+      private void ValidateColumn(int columnIndex)
+      {
+         if (columnIndex < 0 || columnIndex >= _columns)
+            throw new ArgumentOutOfRangeException($"{nameof(columnIndex)} = {columnIndex} is out of bounds [0, {_columns}).");
+      }
+
+      /// <summary>
+      /// Gets or sets the specified Bit in the Matrix
+      /// </summary>
+      /// <param name="rowIndex">The row to operate on.</param>
+      /// <param name="columnIndex">The column to operate on.</param>
+      /// <returns>True if the bit is set; false if the bit is clear.</returns>
+      public bool this[int rowIndex, int columnIndex]
+      {
+         get
+         {
+            ValidateRow(rowIndex);
+            ValidateColumn(columnIndex);
+            return _rows[rowIndex][columnIndex];
+         }
+         set
+         {
+            ValidateRow(rowIndex);
+            ValidateColumn(columnIndex);
+            _rows[rowIndex][columnIndex] = value;
+         }
+      }
+
+      /// <summary>
+      /// Flips the specified bit.
+      /// </summary>
+      /// <param name="rowIndex">The row in which to flip the bit.</param>
+      /// <param name="columnIndex">The column in which to flip the bit.</param>
+      public void FlipBit(int rowIndex, int columnIndex)
+      {
+         ValidateRow(rowIndex);
+         ValidateColumn(columnIndex);
+         _rows[rowIndex].FlipBit(columnIndex);
+      }
+   }
+}
---
+++ Test.Library/QuadraticSieve/TestMatrix.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Friendly.Library;
+using Friendly.Library.QuadraticSieve;
+using Xunit;
+
+namespace Test.Library.QuadraticSieve
+{
+   public class TestMatrix
+   {
+      public TestMatrix()
+      {
+      }
+
+      [Fact]
+      public void ctor()
+      {
+         int rowCount = 64;
+         int columnCount = 128;
+         int bufferCount = 64;
+         Matrix matrix = new Matrix(rowCount, columnCount, bufferCount);
+
+         Assert.Equal(rowCount, matrix.Rows);
+         Assert.Equal(columnCount, matrix.Columns);
+
+         for (int r = 0; r < rowCount; r++)
+            for (int c = 0; c < columnCount; c++)
+               Assert.False(matrix[r, c]);
+      }
+
+      public static TheoryData<int, int, int> ExpandTestData
+      {
+         get
+         {
+            var rv = new TheoryData<int, int, int>();
+
+            // Test where expansion fits into the buffer.
+            rv.Add(32, 32, 33);
+
+            // Test where expansion overflows the buffer.
+            rv.Add(32, 32, 96);
+
+            return rv;
+         }
+      }
+
+      [Theory]
+      [MemberData(nameof(ExpandTestData))]
+      public void Expand(int initialColumnCount, int bufferCount, int expandedColumnCount)
+      {
+         int rowCount = 32;
+         Matrix matrix = new Matrix(rowCount, initialColumnCount, bufferCount);
+
+         // Initialize with a checkerboard pattern
+         for (int r = 0; r < rowCount; r++)
+            for (int c = 0; c < initialColumnCount; c++)
+               matrix[r, c] = ((r + c) & 1) == 0;
+
+         matrix.ExpandColumns(expandedColumnCount);
+
+         Assert.Equal(rowCount, matrix.Rows);
+         Assert.Equal(expandedColumnCount, matrix.Columns);
+
+         // Confirm pre-existing Matrix entries are unchanged
+         for (int r = 0; r < rowCount; r++)
+            for (int c = 0; c < initialColumnCount; c++)
+               Assert.Equal(((r + c) & 1) == 0, matrix[r, c]);
+
+         // Confirm expanded portion of Matrix is all false
+         for (int r = 0; r < rowCount; r++)
+            for (int c = initialColumnCount; c < expandedColumnCount; c++)
+               Assert.False(matrix[r, c]);
+      }
+
+      [Fact]
+      public void Expand_Shrink_Ignored()
+      {
+         int rowCount = 32;
+         int columnCount = 64;
+         Matrix matrix = new Matrix(rowCount, columnCount, 0);
+
+         matrix.ExpandColumns(columnCount - 1);
+
+         Assert.Equal(columnCount, matrix.Columns);
+      }
+
+      [Fact]
+      public void Indexer_Throws()
+      {
+         int rowCount = 64;
+         int columnCount = 128;
+
+         Matrix matrix = new Matrix(rowCount, columnCount, 0);
+
+         Assert.Throws<ArgumentOutOfRangeException>(() => matrix[-1, 0]);
+         Assert.Throws<ArgumentOutOfRangeException>(() => matrix[0, -1]);
+         Assert.Throws<ArgumentOutOfRangeException>(() => matrix[rowCount, 0]);
+         Assert.Throws<ArgumentOutOfRangeException>(() => matrix[0, columnCount]);
+
+         bool bit;
+         bool noThrow = true;
+         try
+         {
+            bit = matrix[0, 0];
+            bit = matrix[rowCount - 1, 0];
+            bit = matrix[0, columnCount - 1];
+            bit = matrix[rowCount - 1, columnCount - 1];
+         }
+         catch(ArgumentOutOfRangeException)
+         {
+            noThrow = false;
+         }
+
+         Assert.True(noThrow);
+      }
+
+      [Fact]
+      public void Indexer()
+      {
+         int rowCount = 32;
+         int columnCount = 32;
+
+         Matrix matrix = new Matrix(rowCount, columnCount, 0);
+
+         // Set bits one at a time stepping through the matrix.
+         for (int r = 0; r < rowCount; r ++)
+            for (int c = 0; c < columnCount; c ++)
+            {
+               matrix[r, c] = true;
+               for (int r1 = 0; r1 < rowCount; r1 ++)
+                  for (int c1 = 0; c1 < columnCount; c1 ++)
+                     Assert.Equal(r1 < r || (r1 == r && c1 <= c), matrix[r1, c1]);
+            }
+      }
+
+      [Fact]
+      public void FlipBit()
+      {
+         int rowCount = 32;
+         int columnCount = 32;
+
+         Matrix matrix = new Matrix(rowCount, columnCount, 0);
+
+         // Flip the bits one at a time stepping through the matrix.
+         for (int r = 0; r < rowCount; r++)
+            for (int c = 0; c < columnCount; c++)
+            {
+               matrix.FlipBit(r, c);
+               for (int r1 = 0; r1 < rowCount; r1++)
+                  for (int c1 = 0; c1 < columnCount; c1++)
+                     Assert.Equal(r1 == r && c1 == c, matrix[r1, c1]);
+               matrix.FlipBit(r, c);
+               Assert.False(matrix[r, c]);
+            }
+      }
+   }
+}
