bcd08e9037888039986d57a3e2fb77ad7e1c3c14
mj <mj@Marks-Mac-mini.local>
2022-01-10 11:56:09 -0700

    Added a SquareRoot method for BigInteger

---
+++ Friendly.Library/BigIntegerCalculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+
+namespace Friendly.Library
+{
+   public static class BigIntegerCalculator
+   {
+      /// <summary>
+      /// Calculates an approximate square root of a long
+      /// </summary>
+      /// <returns>A value, say x, such that x * x <= n < (x+1)*(x+1).</returns>
+      /// <remarks>A double has a 53 bit mantissa.  Thus, it may not calculate
+      /// square roots with sufficient accuracy for any integer value greater
+      /// than 2**53.</remarks>
+      public static BigInteger SquareRoot(BigInteger n)
+      {
+         // Estimate rough power of ten of the root.
+         int pow = 0;
+         BigInteger n1 = n;
+         while (n1 > 100)
+         {
+            pow++;
+            n1 /= 100;
+         }
+
+         // Rough initial estimate of square root.
+         int k = 1;
+         while (k < 10 && k * k < n1)
+            k++;
+         BigInteger root = (k > 1 ? k - 1 : 1) * BigInteger.Pow(10, pow);
+
+         // Iterate to the desired value
+         while (root * root > n || (root + 1) * (root + 1) <= n)
+            root = (root + n / root) / 2;
+
+         return root;
+      }
+
+   }
+}
---
+++ Test.Library/TestBigIntegerCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+using Friendly.Library;
+using Xunit;
+
+namespace Test.Library
+{
+   public class TestBigIntegerCalculator
+   {
+      public TestBigIntegerCalculator()
+      {
+      }
+
+      public static TheoryData<long, long> SquareRootTestData
+      {
+         get
+         {
+            var rv = new TheoryData<long, long>();
+
+            // Test that we get the right answer for a perfect square.
+            rv.Add(5_000_000_029, 0);
+
+            // Test that we get the floor (and not the ceiling)
+            // for one less than the next square number.
+            rv.Add(5_000_000_039, 2 * 5_000_000_039);
+
+            return rv;
+         }
+      }
+
+      [Theory]
+      [MemberData(nameof(SquareRootTestData))]
+      public void SquareRoot(long expectedRoot, long offset)
+      {
+         BigInteger value = (new BigInteger(expectedRoot)) * expectedRoot + offset;
+
+         BigInteger actualRoot = BigIntegerCalculator.SquareRoot(value);
+
+         Assert.Equal(expectedRoot, actualRoot);
+      }
+   }
+}
