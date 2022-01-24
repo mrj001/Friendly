7a36320e504651766d1cc2216629eb482ee313fc
mj <mj@Marks-Mac-mini.local>
2022-01-24 14:14:46 -0700

    Created the Polynomial class

---
+++ Friendly.Library/QuadraticSieve/Polynomial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Friendly.Library.QuadraticSieve
+{
+   /// <summary>
+   /// Represents a polynomial of the form: a * x**2 + 2b * x + c
+   /// </summary>
+   public class Polynomial
+   {
+      private readonly long _a;
+      private readonly long _b;
+      private readonly long _c;
+
+      private Polynomial(long a, long b, long c)
+      {
+         _a = a;
+         _b = b << 1;  // 2 * b
+         _c = c;
+      }
+
+      public long A { get => _a; }
+      public long B { get => _b >> 1; }
+      public long C { get => _c; }
+
+      public long Evaluate(long x)
+      {
+         long rv = _a * x;
+         rv += _b;
+         rv *= x;
+         rv += _c;
+
+         return rv;
+      }
+   }
+}
---
+++ Test.Library/QuadraticSieve/TestPolynomial.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Reflection;
+using Friendly.Library;
+using Friendly.Library.QuadraticSieve;
+using Xunit;
+
+namespace Test.Library.QuadraticSieve
+{
+   public class TestPolynomial
+   {
+      public TestPolynomial()
+      {
+      }
+
+      private static Polynomial InvokeConstructor(long a, long b, long c)
+      {
+         ConstructorInfo? info = typeof(Polynomial).GetConstructor(
+                  BindingFlags.NonPublic | BindingFlags.Instance, null,
+                  new Type[] { typeof(long), typeof(long), typeof(long) }, null);
+         if (info is null)
+            throw new ApplicationException("Failed to obtain ConstructorInfo object");
+
+         return (Polynomial)info.Invoke(new object[] { a, b, c });
+      }
+
+      public static TheoryData<long, long, long> ctorTestData
+      {
+         get
+         {
+            var rv = new TheoryData<long, long, long>();
+
+            rv.Add(1, 0, 42);
+            rv.Add(1, 2, 3);
+
+            return rv;
+         }
+      }
+
+      [Theory]
+      [MemberData(nameof(ctorTestData))]
+      public void ctor(long a, long b, long c)
+      {
+         Polynomial actual = InvokeConstructor(a, b, c);
+
+         Assert.Equal(a, actual.A);
+         Assert.Equal(b, actual.B);
+         Assert.Equal(c, actual.C);
+      }
+
+      public static TheoryData<long, long, long, long, long> EvaluateTestData
+      {
+         get
+         {
+            var rv = new TheoryData<long, long, long, long, long>();
+
+            rv.Add(102, 12, 1, 0, -42);
+            rv.Add(7291, 42, 4, 2, 67);
+
+            return rv;
+         }
+      }
+
+      [Theory]
+      [MemberData(nameof(EvaluateTestData))]
+      public void Evaluate(long expected, long x, long a, long b, long c)
+      {
+         Polynomial poly = InvokeConstructor(a, b, c);
+
+         long actual = poly.Evaluate(x);
+
+         Assert.Equal(expected, actual);
+      }
+   }
+}
