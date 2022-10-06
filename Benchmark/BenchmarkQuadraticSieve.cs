865b0b186fa9fa76f57669e294f5dcd214b6ac7b
mj <mj@Marks-Mac-mini.local>
2022-10-06 16:08:29 -0600

    Updating Quadratic Sieve Benchmarks for larger integers

--- Benchmark/BenchmarkQuadraticSieve.cs
+++ Benchmark/BenchmarkQuadraticSieve.cs
@@ -16,37 +16,46 @@ namespace Benchmark
    {
       public BenchmarkQuadraticSieve()
       {
-         // This limit must be at least as big as any factor in _factors.
          long limit = 2_147_483_648;
          if (Primes.SieveLimit < limit)
             Primes.Init(limit);
       }
 
-      private static (long, long)[] _factors = new (long, long)[]
+      private static (BigInteger, BigInteger)[] _factors = new(BigInteger, BigInteger)[]
       {
          (10_247,10_267),
          (105_199, 101_419),
          (1_046_659, 1_001_387),
          (10_240_033, 10_185_761),
          (106_524_623, 100_729_339),
-         (1_092_547_727, 1_066_246_453)
+         (1_092_547_727, 1_066_246_453),             // 5; 10 digits
+         (56_395_346_957, 28_433_673_481),           // 6; 11 digits
+         (519_438_285_917, 881_984_581_069),         // 7; 12 digits
+         (5_197_073_209_099, 3_875_368_659_733),     // 8; 13 digits
+         (22_584_790_169_573, 50_816_358_827_621),   // 9; 14 digits
+         (500_111_274_667_351, 299_456_570_077_393),              // 10; 15 digits
+         (2_664_084_940_031_677, 4_297_751_019_632_717),          // 11; 16 digits
+         (23_806_490_003_715_389, 19_987_728_972_900_803),        // 12; 17 digits
+         (167_028_431_399_642_933, 328_627_472_829_101_449),      // 13; 18 digits
+         (5_626_527_389_734_197_521, 9_631_131_476_434_794_037),  // 14; 19 digits
+         //(BigInteger.Parse("73586197855458995167"), BigInteger.Parse("65387170920028618561")) // 20 digits
       };
 
-      [Params(0,1,2,3,4,5)]
+      [Params(0,1,2,3,4,5, 6, 7, 8, 9, 10, 11, 12, 13, 14)]
       public int FactorIndex { get; set; }
 
       [Benchmark]
       public void RunQuadraticSieve()
       {
-         (long f1, long f2) = _factors[FactorIndex];
+         (BigInteger f1, BigInteger f2) = _factors[FactorIndex];
          if (f1 > f2)
          {
-            long t = f1;
+            BigInteger t = f1;
             f1 = f2;
             f2 = t;
          }
 
-         long n = f1 * f2;
+         BigInteger n = f1 * f2;
          QuadraticSieve sieve = new QuadraticSieve(n);
          (BigInteger g1, BigInteger g2) = sieve.Factor();
 
@@ -58,34 +67,6 @@ namespace Benchmark
          }
 
          if (g1 != f1 || g2 != f2)
-            throw new ApplicationException();
-      }
-
-      [Benchmark]
-      public void RunTrialDivision()
-      {
-         // Since the Quadratic Sieve only finds two factors, we'll stop this
-         // after finding one factor.  The benchmark runs with only two factors
-         // anyway.
-         (long f1, long f2) = _factors[FactorIndex];
-         if (f1 > f2)
-         {
-            long t = f1;
-            f1 = f2;
-            f2 = t;
-         }
-         long product = f1 * f2;
-
-         long g1, g2, r;
-         IEnumerator<long> j = Primes.GetEnumerator();
-         j.MoveNext();
-         do
-         {
-            g1 = j.Current;
-            g2 = Math.DivRem(product, g1, out r);
-         } while (j.MoveNext() && r != 0);
-
-         if (r != 0 || g1 != f1 || g2 != f2)
             throw new ApplicationException();
       }
    }
--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -8,7 +8,7 @@ namespace Benchmark
    {
       public static void Main(string[] args)
       {
-         var summary = BenchmarkRunner.Run<BenchmarkGCD>();
+         var summary = BenchmarkRunner.Run<BenchmarkQuadraticSieve>();
       }
    }
 }
