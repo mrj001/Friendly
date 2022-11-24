f6110e2af19301fd5bb92c0e3eb4e4d1dd425848
mj <mj@Marks-Mac-mini.local>
2022-11-23 18:38:31 -0700

    Optimize: choose Single Large Prime or Two Large Primes

--- Friendly.Library/QuadraticSieve/RelationsFactory.cs
+++ Friendly.Library/QuadraticSieve/RelationsFactory.cs
@@ -11,7 +11,10 @@ namespace Friendly.Library.QuadraticSieve
       public IRelations GetRelations(int numDigits, int factorBaseSize,
          int maxFactor, long maxLargePrime)
       {
-         return new Relations2P(factorBaseSize, maxFactor, maxLargePrime);
+         if (numDigits > 59)
+            return new Relations2P(factorBaseSize, maxFactor, maxLargePrime);
+         else
+            return new Relations(factorBaseSize, maxFactor, maxLargePrime);
       }
    }
 }
