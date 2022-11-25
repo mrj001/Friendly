649312c419e9f792b86d03528a9437b7e024019b
mj <mj@Marks-Mac-mini.local>
2022-11-25 15:53:53 -0700

    Parallelize sieving

--- Friendly.Library/QuadraticSieve/QuadraticSieve.cs
+++ Friendly.Library/QuadraticSieve/QuadraticSieve.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
 using Friendly.Library;
 
 //====================================================================
@@ -59,12 +61,7 @@ namespace Friendly.Library.QuadraticSieve
       private IMatrix _matrix;
       private IMatrixFactory _matrixFactory;
 
-      /// <summary>
-      /// A count of the number of completed iterations of sieving.
-      /// </summary>
-      private int _sieveIntervals;
-
-      private IEnumerator<Polynomial> _polynomials;
+      private IEnumerable<Polynomial> _polynomials;
       private int _totalPolynomials;
 
       public event EventHandler<NotifyProgressEventArgs> Progress;
@@ -88,7 +85,6 @@ namespace Friendly.Library.QuadraticSieve
          _matrix = null;
          _matrixFactory = new MatrixFactory();
 
-         _sieveIntervals = 0;
          _polynomials = null;
          _totalPolynomials = 0;
       }
@@ -135,7 +131,7 @@ namespace Friendly.Library.QuadraticSieve
             pmax, ((long)pmax) * pmax);
 
          _M = _parameters.FindSieveInterval(_n);
-         _polynomials = (new MultiPolynomial(_n, _rootN, _factorBase.MaxPrime, _M)).GetEnumerator();
+         _polynomials = new MultiPolynomial(_n, _rootN, _factorBase.MaxPrime, _M);
 
          FindBSmooth();
 
@@ -245,87 +241,98 @@ namespace Friendly.Library.QuadraticSieve
 
          int numRelationsNeeded = fbSize + 1;
 
-         do
+         // The Relations class is not thread-safe.
+         int maxDegreeOfParallelism;
+         if (_relations is Relations)
+            maxDegreeOfParallelism = 1;
+         else
+            maxDegreeOfParallelism = Environment.ProcessorCount / 2;
+
+         ParallelOptions options = new ParallelOptions();
+         options.MaxDegreeOfParallelism = maxDegreeOfParallelism;
+         options.CancellationToken = (new CancellationTokenSource()).Token;
+
+         // TODO: be able to restart the enumeration of polynomials.
+         Parallel.ForEach<Polynomial>(_polynomials, options,
+            (poly, state) => DoOneSieve(state, poly, numRelationsNeeded, pmaxt,
+            firstPrimeIndex, smallPrimeLog));
+      }
+
+      private void DoOneSieve(ParallelLoopState state, Polynomial poly,
+         int numRelationsNeeded, double pmaxt, int firstPrimeIndex, float smallPrimeLog)
+      {
+         Interlocked.Increment(ref _totalPolynomials);
+         int sieveSize = 2 * _M + 1;
+         ushort[] sieve = new ushort[sieveSize];
+
+         // The Sieve Threshold is per Ref. B, Section 4 (iii),
+         // adjusted for the exclusion of small primes from sieving.
+         ushort sieveThreshold = (ushort)Math.Round(Math.Log(_M * Math.Sqrt((double)_n / 2) / pmaxt) - smallPrimeLog);
+
+         // for all primes in the factor base (other than -1 and 2) add Add log(p) to the sieve
+         for (int j = firstPrimeIndex, jul = _factorBase.Count; j < jul; j++)
          {
-            if (!_polynomials.MoveNext())
-               throw new ApplicationException($"Ran out of polynomials while factoring {_n:N0}\nFactor Base Count: {_factorBase.Count}");
+            FactorBasePrime prime = _factorBase[j];
+            ushort log = (ushort)prime.Log;
 
-            Polynomial poly = _polynomials.Current;
-            _totalPolynomials++;
+            // Find the roots of Q(x) mod p.
+            int curPrime = prime.Prime;
+            int rootnModP = prime.RootNModP;
+            BigInteger inv2a = BigIntegerCalculator.FindInverse(2 * poly.A, curPrime);  // BUG 2 is not invertible
+            int x1 = (int)((-poly.B + rootnModP) * inv2a % curPrime);
+            int x2 = (int)((-poly.B - rootnModP) * inv2a % curPrime);
 
-            // Initialize the Sieve array to zero over the range [-M, M]
-            for (int j = 0; j < sieveSize; j++)
-               sieve[j] = 0;
+            // Translate to the first index of Q and exponentVectors where
+            // the values will divide evenly
+            int offset = (int)(_M % curPrime);
+            int index1 = x1 + offset;
+            if (index1 < 0) index1 += curPrime;
+            if (index1 >= curPrime) index1 -= curPrime;
+            int index2 = x2 + offset;
+            if (index2 < 0) index2 += curPrime;
+            if (index2 >= curPrime) index2 -= curPrime;
 
-            // The Sieve Threshold is per Ref. B, Section 4 (iii),
-            // adjusted for the exclusion of small primes from sieving.
-            ushort sieveThreshold = (ushort)Math.Round(Math.Log(_M * Math.Sqrt((double)_n / 2) / pmaxt) - smallPrimeLog);
+            // Add the Log of the Prime to each r +/- p location.
+            AddLogs(sieve, index1, curPrime, log);
+            AddLogs(sieve, index2, curPrime, log);
+         }
 
-            // for all primes in the factor base (other than -1 and 2) add Add log(p) to the sieve
-            for (int j = firstPrimeIndex, jul = _factorBase.Count; j < jul; j ++)
+         // Find all sieve locations which exceed the Sieve Threshold
+         for (int x = -_M, idx = 0; x <= _M; x++, idx++)
+         {
+            if (sieve[idx] > sieveThreshold)
             {
-               FactorBasePrime prime = _factorBase[j];
-               ushort log = (ushort)prime.Log;
+               BigBitArray exponentVector = new BigBitArray(_factorBase.Count);
+               BigInteger Q = poly.Evaluate(x);
+               BigInteger origQ = Q;
 
-               // Find the roots of Q(x) mod p.
-               int curPrime = prime.Prime;
-               int rootnModP = prime.RootNModP;
-               BigInteger inv2a = BigIntegerCalculator.FindInverse(2 * poly.A, curPrime);  // BUG 2 is not invertible
-               int x1 = (int)((-poly.B + rootnModP) * inv2a % curPrime);
-               int x2 = (int)((-poly.B - rootnModP) * inv2a % curPrime);
-
-               // Translate to the first index of Q and exponentVectors where
-               // the values will divide evenly
-               int offset = (int)(_M % curPrime);
-               int index1 = x1 + offset;
-               if (index1 < 0) index1 += curPrime;
-               if (index1 >= curPrime) index1 -= curPrime;
-               int index2 = x2 + offset;
-               if (index2 < 0) index2 += curPrime;
-               if (index2 >= curPrime) index2 -= curPrime;
-
-               // Add the Log of the Prime to each r +/- p location.
-               AddLogs(sieve, index1, curPrime, log);
-               AddLogs(sieve, index2, curPrime, log);
-            }
-
-            // Find all sieve locations which exceed the Sieve Threshold
-            for (int x = -_M, idx = 0; x <= _M; x ++, idx++)
-            {
-               if (sieve[idx] > sieveThreshold)
+               // Handle the -1 prime
+               if (Q < 0)
                {
-                  BigBitArray exponentVector = new BigBitArray(_factorBase.Count);
-                  BigInteger Q = poly.Evaluate(x);
-                  BigInteger origQ = Q;
-
-                  // Handle the -1 prime
-                  if (Q < 0)
-                  {
-                     Q *= -1;
-                     exponentVector.FlipBit(0);
-                  }
-
-                  // Handle the remaining primes
-                  for (int j = 1, jul = _factorBase.Count; j < jul; j ++)
-                  {
-                     int curPrime = _factorBase[j].Prime;
-                     BigInteger q, r;
-                     q = BigInteger.DivRem(Q, curPrime, out r);
-                     while (r == 0)
-                     {
-                        Q = q;
-                        exponentVector.FlipBit(j);
-                        q = BigInteger.DivRem(Q, curPrime, out r);
-                     }
-                  }
-
-                  _relations.TryAddRelation(origQ, poly.EvaluateLHS(x), exponentVector, Q);
+                  Q *= -1;
+                  exponentVector.FlipBit(0);
                }
+
+               // Handle the remaining primes
+               for (int j = 1, jul = _factorBase.Count; j < jul; j++)
+               {
+                  int curPrime = _factorBase[j].Prime;
+                  BigInteger q, r;
+                  q = BigInteger.DivRem(Q, curPrime, out r);
+                  while (r == 0)
+                  {
+                     Q = q;
+                     exponentVector.FlipBit(j);
+                     q = BigInteger.DivRem(Q, curPrime, out r);
+                  }
+               }
+
+               _relations.TryAddRelation(origQ, poly.EvaluateLHS(x), exponentVector, Q);
             }
+         }
 
-            _sieveIntervals ++;
-
-         } while (_relations.Count < numRelationsNeeded);
+         if (_relations.Count > numRelationsNeeded)
+            state.Stop();
       }
 
       private static void AddLogs(ushort[] sieve, int startIndex, int stride, ushort log)
@@ -346,15 +353,13 @@ namespace Friendly.Library.QuadraticSieve
       /// </remarks>
       public void PrepareToResieve()
       {
-         if (!_polynomials.MoveNext())
-            throw new ApplicationException("Ran out of polynomials");
-         _totalPolynomials++;
-
-         // Remove the free columns which generated the non-useful null vectors.
-         List<int> freeColumns = _matrix.FindFreeColumns();
-         _matrix = null;
-         for (int j = freeColumns.Count - 1; j >= 0; j--)
-            _relations.RemoveRelationAt(j);
+         // TODO: need to be able to restart enumeration of Polynomials.
+         throw new ApplicationException("No null vector yielded a factorization.");
+         //// Remove the free columns which generated the non-useful null vectors.
+         //List<int> freeColumns = _matrix.FindFreeColumns();
+         //_matrix = null;
+         //for (int j = freeColumns.Count - 1; j >= 0; j--)
+         //   _relations.RemoveRelationAt(j);
       }
    }
 }
