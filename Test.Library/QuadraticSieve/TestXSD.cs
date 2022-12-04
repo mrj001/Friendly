4b204e17046c51c57c6ef0ab44e874556652597a
mj <mj@Marks-Mac-mini.local>
2022-12-04 09:37:03 -0700

    Initial save file format for Quadratic Sieve

---
+++ Test.Library/QuadraticSieve/TestXSD.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Schema;
+using Xunit;
+
+namespace Test.Library.QuadraticSieve
+{
+   public class TestXSD
+   {
+      [Fact]
+      public void Compiles()
+      {
+         XmlSchemaSet schemas = new XmlSchemaSet();
+
+         Assembly assy = AppDomain.CurrentDomain.GetAssemblies().
+            Where(a => !a.IsDynamic && Path.GetFileName(a.Location) == "Friendly.Library.dll").
+                 First();
+
+         using (Stream xsd = assy.GetManifestResourceStream("Friendly.Library.Assets.QuadraticSieve.SaveQuadraticSieve.xsd")!)
+            schemas.Add(XmlSchema.Read(xsd, null)!);
+
+         // If this schema is faulty, this will throw an XmlSchemaException and fail
+         // the test.
+         schemas.Compile();
+      }
+   }
+}
+
